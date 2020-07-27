9440adfc2e10214cdc2fb5f14f6c9875ef017f31
berndt-toast83 <59666708+berndt-toast83@users.noreply.github.com>
2020-07-27 14:31:11 -0500

    Update scryfall_format_cube.py

--- scryfall_format_cube.py
+++ scryfall_format_cube.py
@@ -1,7 +1,7 @@
 from scryfall_formatter import *
 
 # Scan every card in a cube
-page = 0
+page = 1
 cardnames = []
 more = True
 expansion = input("Type the character set code for the cube you want to scan: ")
@@ -10,13 +10,13 @@ copyright = input("Enter Yes to remove the copyright from the scan. Leave blank 
 
 # ensure we get every card from the cube (multiple search result pages)
 while more:
-    time.sleep(0.1)
-    cardset = scrython.cards.Search(q="cube:" + expansion, page=page)
-    more = cardset.has_more()
-    cardnames = cardnames + [cardset.data()[x]["name"] for x in range(len(cardset.data()))]
-    page += 1
+	time.sleep(0.1)
+	cardset = scrython.cards.Search(q="cube:" + expansion, page=page)
+	more = cardset.has_more()
+	cardnames = cardnames + [cardset.data()[x]["name"] for x in range(len(cardset.data()))]
+	page += 1
 
 print("Collected search results for cube: " + expansion)
 
 for cardname in sorted(set(cardnames)):
-    process_card(cardname, expansion=None, advanced=None, holo=holo, copyright=copyright)
+	process_card(cardname, expansion=None, advanced=None, holo=holo, copyright=copyright)
