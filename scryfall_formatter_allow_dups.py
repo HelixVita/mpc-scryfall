1b0eb4fbadb46763943549cb24a77261264e8639
berndt-toast83 <59666708+berndt-toast83@users.noreply.github.com>
2020-07-27 15:21:34 -0500

    Update scryfall_formatter_allow_dups.py

--- scryfall_formatter_allow_dups.py
+++ scryfall_formatter_allow_dups.py
@@ -126,6 +126,12 @@ def process_card_dups(cardname, cardnumber, expansion=None, advanced=None, holo=
 				topPix = 1550
 				bottomPix = 1585
 
+				# story spotlights have a shifted legal line
+					if card_obj["story_spotlight"] is True:
+						topPix = 1585
+						bottomPix = 1625
+						# spotlight card
+
 				# creatures have a shifted legal line
 				try:
 					power = card_obj["power"]
